2b96c1af33b069a90d70512246a2b44ef5ea3880
Levgenix <mr.lobach@gmail.com>
2021-07-14 10:26:16 +0300

    Dubstep initial

---
+++ src/test/java/DubstepTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DubstepTest {
+
+    @Test
+    void songDecoder() {
+        fail();
+    }
+    @Test
+    void stringExceed200Characters() {
+        fail();
+    }
+}
